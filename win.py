222e99c784b5de5523766cd10fd7a4b178ec3594
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-31 19:20:27 +0530

    Update win.py

--- win.py
+++ win.py
@@ -19,10 +19,7 @@ sf = pd.read_csv('flags_iso.csv')
 st.header('T20 MATCH ANALYSIS')
 o=20
 o = st.number_input('Over No.(Not Greater Than Overs Played in 2nd Innings)') or 20
-h = st.text_input(
-    'Enter the URL (ESPN CRICINFO > Select Match > Click On Overs):',
-    'https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/australia-vs-india-51st-match-super-eights-group-1-1415751/match-overs-comparison'
-)
+h = st.text_input('Enter the URL (ESPN CRICINFO > Select Match > Click On Overs):') or str('https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/australia-vs-india-51st-match-super-eights-group-1-1415751/match-overs-comparison')
 
 if h == 'https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/australia-vs-india-51st-match-super-eights-group-1-1415751/match-overs-comparison':
     st.write('Enter Your URL')
