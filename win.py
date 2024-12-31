48dca6e1943ab1d4b4dab7e735c8f11ccd374ed2
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-31 16:23:42 +0530

    Update win.py

--- win.py
+++ win.py
@@ -272,6 +272,7 @@ fig.update_layout(
         "tickvals": [-10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
         "ticktext": [gf['bowlingTeam_x'].values[0], "100%", "90%", "80%", "70%", "60%", "50%", 
                      "60%", "70%", "80%", "90%", "100%", gf['battingTeam_x'].values[0]]
+        "showgrid": False 
     },
     showlegend=False
 )
