390d1a15977edfd1023c2ef3e75de3a88cdf976b
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2025-01-16 14:18:30 +0530

    Update hr.py

--- hr.py
+++ hr.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 df=pd.read_csv('CompanyWise.csv')
 st.selectbox('Company Name',df['Company'].values)
-st.write(df[df['Name']])
-st.write(df[df['Email']])
+st.write(df[df['Name']].values[0])
+st.write(df[df['Email']].values[0])
